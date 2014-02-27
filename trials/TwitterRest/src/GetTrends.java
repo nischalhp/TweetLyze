cdecd6bd92e42de663f0ad2a9158ceeb1f292374
Nischal HP <n.hp@sap.com>
2014-02-27 17:01:00 +0530

    Added object pool,logger,locations,property files

--- trials/TwitterRest/src/GetTrends.java
+++ trials/TwitterRest/src/GetTrends.java
@@ -2,9 +2,9 @@ import helpers.PropertyHandler;
 
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.ArrayList;
 
 import oauth.signpost.OAuthConsumer;
-import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
 import oauth.signpost.exception.OAuthCommunicationException;
 import oauth.signpost.exception.OAuthExpectationFailedException;
 import oauth.signpost.exception.OAuthMessageSignerException;
@@ -15,80 +15,70 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
-public class GetTrends  {
-   
+public class GetTrends {
     
-
+    private static Logger log;
+    private static String propertiesMain = "/properties/property.properties";
     
     public GetTrends() {
     
     }
     
-    public void buildConsumerPool() {
+    public ArrayList<String> GetTrends(OAuthConsumer consumer) throws OAuthMessageSignerException, OAuthExpectationFailedException, OAuthCommunicationException, ClientProtocolException, IOException {
+    
+        //Output is an arraylist of trends
+        ArrayList<String> trends = null;
         
-        //setting the config path
-        PropertyHandler.setConfigPath("/twitter.properties");
+        //Setting the property file handler and retrieving url and places
+        PropertyHandler.setConfigPath(propertiesMain);
+        String searchTrendUrl = PropertyHandler.getProperty("searchTrends");
+        String cities = PropertyHandler.getProperty("places");
+        String[] city = cities.split(",");
+       
+        PropertyConfigurator.configure(PropertyHandler.getProperty("logPath"));  
         
-        //fetching required tokens for all apps
-        String consumerKeySet = PropertyHandler.getProperty("consumerKey");
-        String consumerSecretSet = PropertyHandler.getProperty("consumerSecret");
-        String accessTokenSet = PropertyHandler.getProperty("accessToken");
-        String tokenSecretSet = PropertyHandler.getProperty("tokenSecret");
-        
-        String[] splitconsumerKeys = consumerKeySet.split(",");
-        String[] splitconsumerSecret = consumerSecretSet.split(".");
-        String[] splitaccessToken = accessTokenSet.split(",");
-        String[] splittokenSecret = tokenSecretSet.split(".");
-        
-        //creating consumer objects for each app
-        for (int numberOfAccounts = 0; numberOfAccounts < splitconsumerKeys.length; numberOfAccounts++) {
+        //Builing request objects for every city and getting the trends
+        for (int numberOfCities = 0; numberOfCities < city.length; numberOfCities++) {
+
+            //request object created
+            HttpGet request = new HttpGet(searchTrendUrl+city[numberOfCities]);
+            consumer.sign(request);
+            HttpClient client = new DefaultHttpClient();
+            HttpResponse response = client.execute(request);
             
-                String consumerKey = splitconsumerKeys[numberOfAccounts];
-                String consumerSecret = splitconsumerSecret[numberOfAccounts];
-                String accessToken = splitaccessToken[numberOfAccounts];
-                String tokenSecret = splittokenSecret[numberOfAccounts];
-        OAuthConsumer consumer = new CommonsHttpOAuthConsumer(consumerKey, consumerSecret);
-        consumer.setTokenWithSecret(accessToken, tokenSecret);
-                
+            int statusCode = response.getStatusLine().getStatusCode();
+            System.out.println(response.getStatusLine().getReasonPhrase());
+            //System.out.println(IOUtils.toString(response.getEntity().getContent()));
+            StringWriter writer = new StringWriter();
+            IOUtils.copy(response.getEntity().getContent(), writer);
+            String jsonStr = writer.toString();
+            //stem.out.println(jsonStr);
+            JSONArray jsonArray = new JSONArray(jsonStr);
+            JSONObject jsonObj = jsonArray.getJSONObject(0);
+            JSONArray jsonArrayTrends = jsonObj.getJSONArray("trends");
+            for (int i = 0; i < jsonArrayTrends.length(); i++) {
+                JSONObject jsonObjTrend = jsonArrayTrends.getJSONObject(i);
+                System.out.println(jsonObjTrend);
+                String searchUrl = jsonObjTrend.getString("url");
+                System.out.println(searchUrl);
+                searchUrl = searchUrl.replaceAll("/search", "/search/tweets.json");
+                searchUrl = searchUrl.replaceAll("//twitter.com", "//api.twitter.com/1.1");
+                searchUrl = searchUrl.replaceAll("http", "https");
+                searchUrl = searchUrl + "&lang=en";
+                System.out.println(searchUrl);
+                SearchTrends trendsQueryObj = new SearchTrends(consumer, searchUrl);
+                break;
+            }
+            
         }
     }
     
     public static void main(String args[]) {
-        
-                HttpGet request = new HttpGet("https://api.twitter.com" + "/1.1/trends/place.json?id=2295420");
-                
-                consumer.sign(request);
-                
-                HttpClient client = new DefaultHttpClient();
-                HttpResponse response = client.execute(request);
-                
-                int statusCode = response.getStatusLine().getStatusCode();
-                System.out.println(response.getStatusLine().getReasonPhrase());
-                //System.out.println(IOUtils.toString(response.getEntity().getContent()));
-                StringWriter writer = new StringWriter();
-                IOUtils.copy(response.getEntity().getContent(), writer);
-                String jsonStr = writer.toString();
-                //stem.out.println(jsonStr);
-                JSONArray jsonArray = new JSONArray(jsonStr);
-                JSONObject jsonObj = jsonArray.getJSONObject(0);
-                JSONArray jsonArrayTrends = jsonObj.getJSONArray("trends");
-                for (int i = 0; i < jsonArrayTrends.length(); i++) {
-                    JSONObject jsonObjTrend = jsonArrayTrends.getJSONObject(i);
-                    System.out.println(jsonObjTrend);
-                    String searchUrl = jsonObjTrend.getString("url");
-                    System.out.println(searchUrl);
-                    searchUrl = searchUrl.replaceAll("/search", "/search/tweets.json");
-                    searchUrl = searchUrl.replaceAll("//twitter.com", "//api.twitter.com/1.1");
-                    searchUrl = searchUrl.replaceAll("http", "https");
-                    searchUrl = searchUrl + "&lang=en";
-                    System.out.println(searchUrl);
-                    SearchTrends trendsQueryObj = new SearchTrends(consumer, searchUrl);
-                    break;
-                }
-                
-        }
+    
+    }
 }

1e11d25ce44e6dc296a555e037aead9cfe0d2d47
Nischal HP <n.hp@sap.com>
2014-03-02 21:58:54 +0530

    Adding logger to the main class

--- trials/TwitterRest/src/main/iAmController.java
+++ trials/TwitterRest/src/main/iAmController.java
@@ -1,21 +1,28 @@
 package main;
 
+import helpers.PropertyHandler;
+
 import java.util.concurrent.BlockingQueue;
 
 import oauth.signpost.OAuthConsumer;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 public class iAmController {
     
-    public static void main(String args[]){
-        
+    private static String propertiesMain = "/properties/property.properties";
+    
+    public static void main(String args[]) {
+    
+        PropertyHandler.setConfigPath(propertiesMain);
         ConsumerPool consumerObjects = new ConsumerPool();
         BlockingQueue<OAuthConsumer> consumerPool = consumerObjects.buildConsumerPool();
+        String logPath = PropertyHandler.getProperty("logPath");
         
-        
-        
-        
-        
+        PropertyConfigurator.configure(logPath);
+        Logger log = Logger.getLogger(iAmController.class.getName());
+        log.info("consumer pool created");
         
     }
     

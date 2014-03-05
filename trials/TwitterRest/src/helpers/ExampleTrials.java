6fe4a18481f113b3024a71ae59c61f17bab20b6c
rd <rd@rd-VirtualBox.(none)>
2014-03-05 20:18:29 +0530

    Adding postgres connector trail example

--- trials/TwitterRest/src/helpers/ExampleTrials.java
+++ trials/TwitterRest/src/helpers/ExampleTrials.java
@@ -1,12 +1,17 @@
 package helpers;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Calendar;
 
 public class ExampleTrials {
     
-    public static void main(String args[]) {
+    public static void main(String args[]) throws SQLException {
     
         getSystemTime();
+        connectToPostgresServer();
     }
     
     public static void getSystemTime() {
@@ -17,5 +22,28 @@ public class ExampleTrials {
         System.out.println(cal.getTime().getHours());
 
         
+    }
+    
+    public static void connectToPostgresServer() throws SQLException{
+    	String hostName = "localhost";
+    	String port = "5433";
+    	String dbName = "tweetyfox";
+    	String uname = "postgres";
+    	String pwd = "tiger";
+    	
+    	Connection conn = null;
+    	conn = DriverManager.getConnection("jdbc:postgresql://"+hostName+":"+port+"/"+dbName,uname,pwd);
+    	if(conn!=null){
+    		System.out.println("Connection is succesfull");
+    		String q = "INSERT INTO location (id,city)"+ "VALUES (1235,'BANGALORE')";
+    		Statement stmt = conn.createStatement();
+    		int i = stmt.executeUpdate(q);
+    		if(i==0){
+    			System.out.println("Error");
+    		}
+    	}else{
+    		System.out.println("Failed, FUCK OFF");
+    	}
+    	conn.close();
     }
 }

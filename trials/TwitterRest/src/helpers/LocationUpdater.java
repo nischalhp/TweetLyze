891437e54e800f6391989779abecc4ed39bec21a
nischalhp <nischal.hp@gmail.com>
2014-03-07 13:52:37 +0530

    Got the connection object from MrPostgres

--- trials/TwitterRest/src/helpers/LocationUpdater.java
+++ trials/TwitterRest/src/helpers/LocationUpdater.java
@@ -1,38 +1,59 @@
 package helpers;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 public class LocationUpdater {
 
 	private static String propertiesMain = "properties/property.properties";
-	
+
 	// updating the postgres locations db
-	public static void updateLocationTable() throws IOException {
+	public static void updateLocationTable() {
 
-		PropertyHandler.setConfigPath(propertiesMain);
-		String places = PropertyHandler.getProperty("places");
-		String locations = PropertyHandler.getProperty("geoId");
-		String[] place = places.split(",");
-		String[] location = locations.split(",");
-
-		for (int numberOfLocations = 0; numberOfLocations < place.length; numberOfLocations++) {
-
-//			System.out.println("Place name is " + place[numberOfLocations]
-//					+ " and the geoid is " + location[numberOfLocations]);
+		Logger log = null;
+		try {
+			PropertyHandler.setConfigPath(propertiesMain);
+			String places = PropertyHandler.getProperty("places");
+			String locations = PropertyHandler.getProperty("geoId");
+			String[] place = places.split(",");
+			String[] location = locations.split(",");
 
 
+			String logPath = PropertyHandler.getProperty("logPath");
+			PropertyConfigurator.configure(logPath);
+			log = Logger.getLogger(LocationUpdater.class.getName());
+
+			String dbTables = PropertyHandler.getProperty("dbTables");
+			PropertyHandler.setConfigPath(dbTables);
+
+			for (int numberOfLocations = 0; numberOfLocations < place.length; numberOfLocations++) {
+
+				// System.out.println("Place name is " +
+				// place[numberOfLocations]
+				// + " and the geoid is " + location[numberOfLocations]);
+
+				// get a postGres connection object
+				MrPostgres postgresObj = new MrPostgres();
+				Connection conn = postgresObj.getPostGresConnection();
+				log.info("Connection object to postgres database recieved");
+				Statement stmt = conn.createStatement();
+				String locationTableName = PropertyHandler.getProperty("location");
+				System.out.println(locationTableName);
+				break;
+			}
+		} catch (SQLException e) {
+			log.error("Properties file not found");
 		}
 	}
 
 	public static void main(String args[]) {
 
-		try {
 			updateLocationTable();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 }

d8f9c6f3adcb3c76350402ed27fe3cf905afcce5
nischalhp <nischal.hp@gmail.com>
2014-03-10 11:26:45 +0530

    Full functional code that updates to postgres db

--- trials/TwitterRest/src/helpers/LocationUpdater.java
+++ trials/TwitterRest/src/helpers/LocationUpdater.java
@@ -1,10 +1,13 @@
 package helpers;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
@@ -18,20 +21,20 @@ public class LocationUpdater {
 
 		Logger log = null;
 		try {
-			PropertyHandler.setConfigPath(propertiesMain);
-			String places = PropertyHandler.getProperty("places");
-			String locations = PropertyHandler.getProperty("geoId");
+			Properties mainProperties = new Properties();
+			mainProperties.load(new FileInputStream(propertiesMain));
+			String places = mainProperties.getProperty("places");
+			String locations = mainProperties.getProperty("geoId");
 			String[] place = places.split(",");
 			String[] location = locations.split(",");
 
-
-			String logPath = PropertyHandler.getProperty("logPath");
+			String logPath = mainProperties.getProperty("logPath");
 			PropertyConfigurator.configure("properties/log.properties");
 			log = Logger.getLogger(LocationUpdater.class.getName());
 
-			String dbTables = PropertyHandler.getProperty("dbTables");
-			PropertyHandler.setConfigPath(dbTables);
-
+			String dbTables = mainProperties.getProperty("dbTables");
+			Properties dbProperties = new Properties();
+			dbProperties.load(new FileInputStream(dbTables));
 			for (int numberOfLocations = 0; numberOfLocations < place.length; numberOfLocations++) {
 
 				// System.out.println("Place name is " +
@@ -39,22 +42,29 @@ public class LocationUpdater {
 				// + " and the geoid is " + location[numberOfLocations]);
 
 				// get a postGres connection object
-				MrPostgres postgresObj = new MrPostgres();
-				Connection conn = postgresObj.getPostGresConnection();
-				System.out.println(conn);
+				Connection conn = MrPostgres.getPostGresConnection();
 				log.info("Connection object to postgres database recieved");
-				Statement stmt = conn.createStatement();
-				String locationTableName = PropertyHandler.getProperty("location");
-				System.out.println(locationTableName);
-				break;
+				String locationTableName = dbProperties.getProperty("location");
+				String queryToInsert = "INSERT INTO location(id,city) values(?,?)";
+				PreparedStatement stmt = conn.prepareStatement(queryToInsert);
+				stmt.setString(1, location[numberOfLocations]);
+				stmt.setString(2, place[numberOfLocations]);
+				log.info("insert query " + stmt.toString());
+				stmt.executeUpdate();
+				conn.close();
+
 			}
 		} catch (SQLException e) {
-			log.error("Properties file not found");
+			log.error(" Something went wrong while creating connectin to postgres or inserting to location table in postgres");
+		} catch (FileNotFoundException e) {
+			log.error("Properties file for DB not found");
+		} catch (IOException e) {
+			log.error("Something went wrong while reading db properties file");
 		}
 	}
 
 	public static void main(String args[]) {
 
-			updateLocationTable();
+		updateLocationTable();
 	}
 }

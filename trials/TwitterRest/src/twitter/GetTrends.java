2a06d3a7ff946577658d6ad2ab5cc136883efdce
nischalhp <nischal.hp@gmail.com>
2014-03-11 22:13:31 +0530

    Updating code to write trends to a table

--- trials/TwitterRest/src/twitter/GetTrends.java
+++ trials/TwitterRest/src/twitter/GetTrends.java
@@ -2,6 +2,7 @@ package twitter;
 import helpers.PropertyHandler;
 
 import java.io.IOException;
+import java.io.StringWriter;
 import java.util.ArrayList;
 
 import oauth.signpost.OAuthConsumer;
@@ -9,6 +10,7 @@ import oauth.signpost.exception.OAuthCommunicationException;
 import oauth.signpost.exception.OAuthExpectationFailedException;
 import oauth.signpost.exception.OAuthMessageSignerException;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
@@ -16,6 +18,9 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class GetTrends {
     
@@ -27,7 +32,7 @@ public class GetTrends {
     }
     
     public ArrayList<String> GetTrends(OAuthConsumer consumer) throws OAuthMessageSignerException, OAuthExpectationFailedException,
-            OAuthCommunicationException, ClientProtocolException, IOException {
+            OAuthCommunicationException, ClientProtocolException, IOException, JSONException {
     
         //Output is an arraylist of trends
         ArrayList<String> trends = null;
@@ -36,10 +41,13 @@ public class GetTrends {
         //Setting the property file handler and retrieving url and places
         PropertyHandler.setConfigPath(propertiesMain);
         String searchTrendUrl = PropertyHandler.getProperty("searchTrends");
-        String cities = PropertyHandler.getProperty("places");
-        String[] city = cities.split(",");
         
         PropertyConfigurator.configure(PropertyHandler.getProperty("logPath"));
+       
+        
+        // Get postgres connection and call the location tables to get the codes
+        
+        MrPostgres postGresConnection = new MrPostgres();
         
         //Builing request objects for every city and getting the trends
         for (int numberOfCities = 0; numberOfCities < city.length; numberOfCities++) {
@@ -53,32 +61,29 @@ public class GetTrends {
             //getting status code 
             int statusCode = response.getStatusLine().getStatusCode();
             System.out.println(response.getStatusLine().getReasonPhrase());
-            //            StringWriter writer = new StringWriter();
-            //            IOUtils.copy(response.getEntity().getContent(), writer);
-            //            String jsonStr = writer.toString();
-            //            //stem.out.println(jsonStr);
-            //            JSONArray jsonArray = new JSONArray(jsonStr);
-            //            JSONObject jsonObj = jsonArray.getJSONObject(0);
-            //            JSONArray jsonArrayTrends = jsonObj.getJSONArray("trends");
-            //            for (int i = 0; i < jsonArrayTrends.length(); i++) {
-            //                JSONObject jsonObjTrend = jsonArrayTrends.getJSONObject(i);
-            //                System.out.println(jsonObjTrend);
-            //                String searchUrl = jsonObjTrend.getString("url");
-            //                System.out.println(searchUrl);
-            //                searchUrl = searchUrl.replaceAll("/search", "/search/tweets.json");
-            //                searchUrl = searchUrl.replaceAll("//twitter.com", "//api.twitter.com/1.1");
-            //                searchUrl = searchUrl.replaceAll("http", "https");
-            //                searchUrl = searchUrl + "&lang=en";
-            //                System.out.println(searchUrl);
-            //                SearchTrends trendsQueryObj = new SearchTrends(consumer, searchUrl);
-            //                break;
-            //            }
-            //            
+                        StringWriter writer = new StringWriter();
+                        IOUtils.copy(response.getEntity().getContent(), writer);
+                        String jsonStr = writer.toString();
+                        JSONArray jsonArray = new JSONArray(jsonStr);
+                        JSONObject jsonObj = jsonArray.getJSONObject(0);
+                        JSONArray jsonArrayTrends = jsonObj.getJSONArray("trends");
+                        for (int i = 0; i < jsonArrayTrends.length(); i++) {
+                            JSONObject jsonObjTrend = jsonArrayTrends.getJSONObject(i);
+                            System.out.println(jsonObjTrend);
+                            //String searchUrl = jsonObjTrend.getString("url");
+                        }
+                        
         }
         return trends;
     }
     
-    public static void main(String args[]) {
+/*                            System.out.println(searchUrl);
+                            searchUrl = searchUrl.replaceAll("/search", "/search/tweets.json");
+                            searchUrl = searchUrl.replaceAll("//twitter.com", "//api.twitter.com/1.1");
+                            searchUrl = searchUrl.replaceAll("http", "https");
+                            searchUrl = searchUrl + "&lang=en";
+                            System.out.println(searchUrl);
+*/    public static void main(String args[]) {
     
     }
 }

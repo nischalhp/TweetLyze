65f4902fbaae252846885fa653eb75841f8ed6a4
nischalhp <nischal.hp@gmail.com>
2014-03-18 09:19:38 +0530

    Job stack builder

---
+++ trials/TwitterRest/src/helpers/MrMestri.java
@@ -0,0 +1,21 @@
+package helpers;
+
+import java.util.Stack;
+
+/*
+ This class will help in building the job stack 
+ that will be used by threads to work on
+ Mestri in kannada means a person who assigns jobs 
+ to people in construction business
+
+*/public class MrMestri {
+
+	public static Stack<String> buildJobs(){
+	
+			Stack<String> jobStack = new Stack<String>();
+		
+		
+	}
+	
+	
+}

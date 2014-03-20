aadf1e747815f04c27c954be73ab1bd7048b02b9
nischalhp <nischal.hp@gmail.com>
2014-03-20 21:48:56 +0530

    Search url component built

--- trials/TwitterRest/src/helpers/MrMestri.java
+++ trials/TwitterRest/src/helpers/MrMestri.java
@@ -3,50 +3,92 @@ package helpers;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
 import java.util.Properties;
 import java.util.Stack;
 
 import org.apache.log4j.Logger;
+import org.apache.http.client.utils.URLEncodedUtils;
 
 /*
  This class will help in building the job stack 
  that will be used by threads to work on
  Mestri in kannada means a person who assigns jobs 
  to people in construction business
-*/
+ */
 
 public class MrMestri {
 	private static String propertiesMain = "properties/property.properties";
 
-	public static Stack<String> buildJobs(){
-	
-			Stack<String> jobStack = new Stack<String>();
-			Logger log = null;
+	public static Stack<String> buildJobs() {
 
-			try{
+		Stack<String> jobStack = new Stack<String>();
+		Logger log = null;
+
+		try {
 			Properties PropertyHandler = new Properties();
 			PropertyHandler.load(new FileInputStream(propertiesMain));
 			String logPath = PropertyHandler.getProperty("logPath");
 			log = Logger.getLogger(MrMestri.class.getName());
 			Connection postgresConn = MrPostgres.getPostGresConnection();
 
-			if(postgresConn!=null){
+			String dbTables = PropertyHandler.getProperty("dbTables");
+			Properties dbTablesPropertyHandler = new Properties();
+			dbTablesPropertyHandler.load(new FileInputStream(dbTables));
+			log.info("db tables property file loaded");
+
+			if (postgresConn == null) {
 				log.error("Postgres connection object could not be instantiated");
-			}
-			else{
+			} else {
 				log.info("Connection object to postgres created successfully");
 			}
-			
-			
-			
-			}catch(FileNotFoundException e){
-				log.error("Properties file path error");
-			} catch (IOException e) {
-				log.error(e.getMessage());
+
+			String selectTrends = "SELECT trend,date from "
+					+ dbTablesPropertyHandler.getProperty("trends")
+					+ " where date = '" + Utilities.getCurrentDate() + "'";
+
+			log.info(selectTrends);
+
+			PreparedStatement stmt = postgresConn
+					.prepareStatement(selectTrends);
+
+			ResultSet rs = stmt.executeQuery();
+			if (rs == null) {
+				log.error("Was unable to fetch trends for the date"
+						+ Utilities.getCurrentDate()
+						+ " , something has gone wrong ");
+			} else {
+				log.info("Building the stack of jobs");
+				while (rs.next()) {
+					String trend = rs.getString(1);
+					String searchUrl = PropertyHandler.getProperty("searchUrl");
+					trend = URLEncoder.encode(trend, "ISO-8859-1");
+					searchUrl = searchUrl + trend + "&lang=en";
+
+					URL searchURI = new URL(searchUrl);
+
+				}
+
 			}
-		
+			postgresConn.close();
+		} catch (FileNotFoundException e) {
+			log.error("Properties file path error");
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		} catch (ParseException e) {
+			log.error("Error while parsing the date");
+		} catch (SQLException e) {
+			log.error(e.getMessage());
+		}
+
+		return jobStack;
 	}
-	
-	
+
 }

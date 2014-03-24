4da20c9177c9bcc068bf650e69a81a671f03679c
nischalhp <nischal.hp@gmail.com>
2014-03-24 23:11:57 +0530

    Added timer and handling of threads and waiting

--- trials/TwitterRest/src/main/JuliusCaesar.java
+++ trials/TwitterRest/src/main/JuliusCaesar.java
@@ -2,31 +2,22 @@ package main;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.sql.SQLException;
-import java.text.ParseException;
-import java.util.Calendar;
+import java.net.URL;
 import java.util.Properties;
+import java.util.Stack;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import oauth.signpost.OAuthConsumer;
-import oauth.signpost.exception.OAuthCommunicationException;
-import oauth.signpost.exception.OAuthExpectationFailedException;
-import oauth.signpost.exception.OAuthMessageSignerException;
 
-import org.apache.http.HttpException;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
-import org.json.JSONException;
-
-import twitter.GetTrends;
 
 public class JuliusCaesar {
 
 	private static String propertiesMain = "properties/property.properties";
+	public static BlockingQueue<OAuthConsumer> consumerPool;
 
 	public static void main(String args[]) {
 
@@ -35,77 +26,89 @@ public class JuliusCaesar {
 		try {
 			PropertyHandler.load(new FileInputStream(propertiesMain));
 
-			BlockingQueue<OAuthConsumer> consumerPool = ConsumerPool
-					.buildConsumerPool();
+			consumerPool = ConsumerPool.buildConsumerPool();
 			String logPath = PropertyHandler.getProperty("logPath");
 
 			PropertyConfigurator.configure(logPath);
 			log = Logger.getLogger(JuliusCaesar.class.getName());
 			log.info("consumer pool created");
 
+			Stack<URL> jobStack = null;
+
+			/*
+			 * Per trend how many calls per hour and waiting time after each
+			 * call
+			 */
+
+			int numberOfTrends = Integer.parseInt(PropertyHandler
+					.getProperty("totalTrends"));
+			int numberOfApps = Integer.parseInt(PropertyHandler
+					.getProperty("numberOfAccounts"));
+			int searchQueries = Integer.parseInt(PropertyHandler
+					.getProperty("numberOfSearchQueries"));
+
+			int totalSearchCalls = numberOfApps * searchQueries;
+			int perTrend = (int) totalSearchCalls / numberOfTrends;
+
+			double timeSpace = 60 / perTrend;
+			double seconds = timeSpace * 60;
+			int milliseconds = (int) seconds * 100;
+
 			// Adding executors
 
-			int NumberOfThreads = 80; 
+			int NumberOfThreads = Integer.parseInt(PropertyHandler
+					.getProperty("numberOfThreads"));
+
 			ExecutorService executor = Executors
 					.newFixedThreadPool(NumberOfThreads);
 			log.info("Executor class has been initialized");
+
 			while (true) {
-				int timeToGetTrends = getSystemTime();
-				if (timeToGetTrends == Integer.parseInt(PropertyHandler
-						.getProperty("trendsTime"))) {
-					// peeking to see if a consumer object is free
-					OAuthConsumer consumerObj = consumerPool.peek();
-					// if its null you need to wait to pick up the object
-					if (consumerObj != null) {
-						// No need to spawn thread , just let the trends do its job
-						GetTrends.retrieveTrends(consumerObj);
-						log.info("Trends for the day has been added to the database");
+
+				MrTimer.startTask();
+				log.info("Started the timer to insert trends");
+
+				jobStack = MrMestri.buildJobs();
+				log.info("Creating a job stack of the search urls");
+				if (jobStack.isEmpty()) {
+					log.info("No trends for the day , have to wait till trends get populated");
+					Thread.sleep(10000);
+				} else {
+					/*
+					 * Now that the job stack is there we need to pick 80 jobs
+					 * at a time and based on number of apps wait to run the
+					 * process again
+					 */
+					int jobToken = 0;
+					while (jobStack.size() > 0) {
+						jobToken++;
+
+						if (jobToken
+								% Integer.parseInt(PropertyHandler
+										.getProperty("totalTrends")) == 0) {
+							log.info("All jobs for the clock cycle complete , waiting for next clock cycle to start. Number of jobs completed "
+									+ jobToken);
+							Thread.sleep(milliseconds);
+						}
+						MrRunnable worker = new MrRunnable(jobStack.pop());
+						executor.execute(worker);
 					}
 				}
-				/*
-				 * based on the number of retrieved trends set time gaps so that
-				 * we do not let them exceed rate limit
-				 */
-				log.info("Creating a job stack of the search urls");
+
 			}
 		} catch (FileNotFoundException e) {
-			log.error("Cant set properties file" + e.getMessage());
-
-		} catch (OAuthMessageSignerException e) {
-			log.error("When signing the request using the consumer object something went wrong "
-					+ e.getMessage());
-
-		} catch (ClientProtocolException e) {
-			log.error("While requesting for data from twitter using the client something went wrong "
-					+ e.getMessage());
-
-		} catch (IOException e) {
-			log.error("Something went wrong while setting properties file"
-					+ e.getMessage());
-
-		} catch (OAuthExpectationFailedException e) {
-			log.info(e.getMessage());
-		} catch (JSONException e) {
-			log.info(e.getMessage());
-		} catch (SQLException e) {
-			log.info(e.getMessage());
-		} 
-		 catch (OAuthCommunicationException e) {
-			log.info(e.getMessage());
-		} catch (HttpException e) {
-			log.info(e.getMessage());
-		} catch (ParseException e) {
-			log.info(e.getMessage());
+			log.error(e);
+		} catch (InterruptedException e) {
+			log.error(e);
 		}
 	}
 
-	public static int getSystemTime() {
-
-		long millis = System.currentTimeMillis();
-		Calendar cal = Calendar.getInstance();
-		cal.setTimeInMillis(millis);
-		return (cal.getTime().getHours());
-
+	public static synchronized OAuthConsumer getConsumerObject() {
+		return consumerPool.peek();
 	}
 
+	public static synchronized void putConsumerObject(OAuthConsumer obj)
+			throws InterruptedException {
+		consumerPool.put(obj);
+	}
 }

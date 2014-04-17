b63352cc6b9f108e8bb1158302ee5e620587abd0
rd <nischal.hp@gmail.com>
2014-04-17 16:08:57 +0530

    List of everyday postgres commands

---
+++ Understandings/essentialQuery.sql
@@ -0,0 +1,22 @@
+﻿select count(*) from tweets
+
+select count(*) from trends
+
+select * from trends where date = '2014-04-17' 
+
+
+
+
+--delete from trends
+
+--insert into trends select * from trendsBackup where date = '2014-04-17' LIMIT 70 OFFSET 0
+
+--create table tweetsBackup as select * from tweets
+
+select * from tweets where trendId in(select id from trends where trend = '#IPL7')
+
+--insert into tweetsBackup select * from tweets
+
+--insert into tweets select * from tweetsBackup
+
+--delete from tweets

808f04b1bcd2237e4074b2c83ef0558b6b0ede0c
rd <nischal.hp@gmail.com>
2014-04-17 16:01:27 +0530

    Testing for multiple loops

--- trials/TwitterRest/src/helpers/ExampleTrials.java
+++ trials/TwitterRest/src/helpers/ExampleTrials.java
@@ -17,7 +17,6 @@ import java.util.Stack;
 import java.util.concurrent.BlockingQueue;
 
 import main.MrMestri;
-import main.MrTimer;
 import main.MrUrl;
 import oauth.signpost.OAuthConsumer;
 import oauth.signpost.exception.OAuthCommunicationException;
@@ -40,36 +39,87 @@ public class ExampleTrials {
 	private static String propertiesMain = "properties/property.properties";
 
 	public static void main(String args[]) throws FileNotFoundException,
-			IOException, SQLException {
+			IOException, SQLException, InterruptedException {
 
-	//	MrTimer.startTask();
-		
+		// MrTimer.startTask();
+
 		// getSystemTime();
-		//connectToPostgresServer();
-		
-		//waitingTimeTest();
+		// connectToPostgresServer();
 
+		// waitingTimeTest();while (true)
+		/*Properties PropertyHandler = new Properties();
+
+		PropertyHandler.load(new FileInputStream(propertiesMain));
+		int numberOfTrends = Integer.parseInt(PropertyHandler
+				.getProperty("totalTrends"));
+		int numberOfApps = Integer.parseInt(PropertyHandler
+				.getProperty("numberOfAccounts"));
+		int searchQueries = Integer.parseInt(PropertyHandler
+				.getProperty("numberOfSearchQueries"));
+
+		int totalSearchCalls = numberOfApps * searchQueries;
+		int perTrend = (int) totalSearchCalls / numberOfTrends;
+
+		double timeSpace = 15 / perTrend;
+		double seconds = timeSpace * 60;
+		int milliseconds = (int) seconds * 1000;
 		Stack<MrUrl> jobs = MrMestri.buildJobs();
+		while (true) {
+			System.out.println("Creating jobs");
+			jobs = MrMestri.buildJobs();
+
+			int jobToken = 0;
+
+			if (jobs.isEmpty()) {
+
+				System.out
+						.println("No trends for the day , have to wait till trends get populated");
+				Thread.sleep(10000);
+			}
+
+			while (jobs.size() > 0) {
+
+				/*
+				 * Now that the job stack is there we need to pick 80 jobs at a
+				 * time and based on number of apps wait to run the process
+				 * again
+				 */
+
+				/*jobs.pop();
+				jobToken++;
+
+				if (jobToken % 70 == 0) {
+					System.out
+							.println("All jobs for the clock cycle complete , waiting for next clock cycle to start. Number of jobs completed "
+									+ jobToken);
+					System.out.println("Job stack size" + jobs.size());
+					Thread.sleep(100);
+
+				}
+			}
+		}*/
 		
-		System.out.println(jobs.pop().getUrl());
-		
-	/*	try {
-			searchTweets(consumerPool.take(), jobs.pop());
-		} catch (OAuthMessageSignerException | OAuthExpectationFailedException
-				| OAuthCommunicationException | IllegalStateException
-				| IOException | JSONException | URISyntaxException
-				| InterruptedException | SQLException | HttpException e) {
-			e.printStackTrace();
-		}
-		
-		
-	consumerPool = ConsumerPool.buildConsumerPool();
-		
-		getTrends();
-		
-		System.out.println(consumerPool.size());
-*/
+	String q = "HTTP/1.1 200 OK [cache-control: no-cache, no-store, must-revalidate, pre-check=0, post-check=0, content-length: 38328, content-type: application/json;charset=utf-8, date: Thu, 17 Apr 2014 09:22:31 GMT, expires: Tue, 31 Mar 1981 05:00:00 GMT, last-modified: Thu, 17 Apr 2014 09:22:31 GMT, pragma: no-cache, server: tfe, set-cookie: guest_id=v1%3A139772655166947606; Domain=.twitter.com; Path=/; Expires=Sat, 16-Apr-2016 09:22:31 UTC, status: 200 OK, strict-transport-security: max-age=631138519, x-access-level: read, x-content-type-options: nosniff, x-frame-options: SAMEORIGIN, x-rate-limit-limit: 180, x-rate-limit-remaining: 142, x-rate-limit-reset: 1397727388, x-transaction: f62b84ce49c9cb84, x-xss-protection: 1; mode=block]";
+	String[] getLimit = q.split("x-rate-limit-remaining:");
+	String[] limit = getLimit[1].split(",");
+	System.out.println(limit[0]);
+
 	}
+
+	/*
+	 * try { searchTweets(consumerPool.take(), jobs.pop()); } catch
+	 * (OAuthMessageSignerException | OAuthExpectationFailedException |
+	 * OAuthCommunicationException | IllegalStateException | IOException |
+	 * JSONException | URISyntaxException | InterruptedException | SQLException
+	 * | HttpException e) { e.printStackTrace(); }
+	 * 
+	 * 
+	 * consumerPool = ConsumerPool.buildConsumerPool();
+	 * 
+	 * getTrends();
+	 * 
+	 * System.out.println(consumerPool.size());
+	 */
 
 	public static void waitingTimeTest() throws FileNotFoundException,
 			IOException {
@@ -84,14 +134,14 @@ public class ExampleTrials {
 				.getProperty("numberOfSearchQueries"));
 
 		int totalSearchCalls = numberOfApps * searchQueries;
-		System.out.println("Total search calls " +totalSearchCalls);
+		System.out.println("Total search calls " + totalSearchCalls);
 		int perTrend = (int) totalSearchCalls / numberOfTrends;
 
-		System.out.println("per trend " +perTrend);
+		System.out.println("per trend " + perTrend);
 		double timeSpace = 15 / perTrend;
-		System.out.println("timeSpace " +timeSpace);
+		System.out.println("timeSpace " + timeSpace);
 		double seconds = timeSpace * 60;
-		System.out.println("seconds" +seconds);
+		System.out.println("seconds" + seconds);
 		int milliseconds = (int) seconds * 1000;
 
 		System.out.println(milliseconds);
@@ -132,13 +182,13 @@ public class ExampleTrials {
 			}
 		}
 	}
-	
-	public static void searchTweets(OAuthConsumer consumer,MrUrl urlObj)
-		
-			throws OAuthMessageSignerException,
-			OAuthExpectationFailedException, OAuthCommunicationException,
-			ClientProtocolException, IOException, IllegalStateException,
-			JSONException, URISyntaxException, SQLException, HttpException, InterruptedException {
+
+	public static void searchTweets(OAuthConsumer consumer, MrUrl urlObj)
+
+	throws OAuthMessageSignerException, OAuthExpectationFailedException,
+			OAuthCommunicationException, ClientProtocolException, IOException,
+			IllegalStateException, JSONException, URISyntaxException,
+			SQLException, HttpException, InterruptedException {
 		/*
 		 * 
 		 * We have the url and the trend id once we get the array of tweets , we
@@ -176,14 +226,12 @@ public class ExampleTrials {
 				+ ":" + port + "/" + dbName, uname, pwd);
 		if (conn != null) {
 			System.out.println("Connection is succesfull");
-			String q = "SELECT * FROM trends"
-					;
+			String q = "SELECT * FROM trends";
 			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery(q);
 			if (rs == null) {
 				System.out.println("Error");
-			}
-			else{
+			} else {
 				System.out.println(rs.next());
 			}
 		} else {

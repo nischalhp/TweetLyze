626a0a4f1638c4d4900e995432f4e3871ff0d3f1
rd <nischal.hp@gmail.com>
2014-05-16 17:41:58 +0530

    Added constructor to the class

--- trials/AnalyzeData/PostgresConnector.py
+++ trials/AnalyzeData/PostgresConnector.py
@@ -8,16 +8,19 @@ class PostgresConnector:
 	# method to read data from config and 
 	# connect to postgres server
 	# and returns the connection object
-	def connect_to_postgres(self):
+	def __init__(self):
 		Config = Configurator.Configurator()
 		ConfiguredObject = Config.read_configuration()
 		Section = "DB Section"
 		parameter_dict = Config.get_parameters(ConfiguredObject,Section)
-		host = parameter_dict['host']
-		dbname = parameter_dict['dbname']
-		user = parameter_dict['user']
-		pwd = parameter_dict['password']
-		conn = psycopg2.connect(dbname=dbname,host=host,password=pwd,user=user) 
+		self.host = parameter_dict['host']
+		self.dbname = parameter_dict['dbname']
+		self.user = parameter_dict['user']
+		self.pwd = parameter_dict['password']
+
+
+	def get_connection(self):
+		conn = psycopg2.connect(dbname=self.dbname,host=self.host,password=self.pwd,user=self.user) 
 		return conn
 
 

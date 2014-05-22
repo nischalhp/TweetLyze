63e39ba0da5966b50f7717bed98f5ffbf26d537d
Shrayas <shrayasr@gmail.com>
2014-05-22 23:40:00 +0530

    Changing file operations to use `with`
    * Open and Close is always flaky. We tend to forget. With is a better
      way to do these things

--- trials/AnalyzeData/IdEntityIndexer.py
+++ trials/AnalyzeData/IdEntityIndexer.py
@@ -14,19 +14,19 @@ class IdEntityIndexer:
 		cursor.execute(query)
 		id_column = 0
 		entities_column = 1
-		io = open('copy_from.txt','wr')
-		for row in cursor:
-			tweet_id = row[id_column]
-			hashtag_array = row[entities_column]
-			hashtag_list = [hashtag['text'] for hashtag in hashtag_array]
-			hashtag_list_unique = list(set(hashtag_list))
-			for hashtag in hashtag_list_unique: 
-				io.write(tweet_id+'\t'+hashtag.encode('utf-8')+'\n')
 
-		io.close()
-		io = open('copy_from.txt','r')
-		cursor.copy_from(io,'"IdEntity"',columns=('id','entity'))	
-		io.close()
+        with open('copy_from.txt','w') as f:
+            for row in cursor:
+                tweet_id = row[id_column]
+                hashtag_array = row[entities_column]
+                hashtag_list = [ hashtag['text'] for hashtag in hashtag_array ]
+                hashtag_list_unique = list(set(hashtag_list))
+                for hashtag in hashtag_list_unique:
+                    f.write(tweet_id + '\t' + hashtag.encode('utf-8') + '\n')
+
+        with open('copy_from.txt') as f:
+            cursor.copy_from(f, '"IdEntity"', columns=('id', 'entity'))
+
 		os.remove('copy_from.txt')
 
 IdEntityIndexer().build()

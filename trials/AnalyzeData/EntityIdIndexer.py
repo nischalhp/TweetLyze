e61b751f279b5afe57ee071411ac7f82be509005
rd <nischal.hp@gmail.com>
2014-05-22 15:47:05 +0530

    Added 2 new methods to get tf and total num of doc
    * Build Tf uses the IdEntity table and generated a dict of entity and tf value
    * Get total documents - gets the total number of unique documents in table

--- trials/AnalyzeData/EntityIdIndexer.py
+++ trials/AnalyzeData/EntityIdIndexer.py
@@ -1,11 +1,12 @@
 #entity id Indexer
 from PostgresConnector import PostgresConnector
+from TfIdf import TfIdf
 class EntityIdIndexer:
 
 	def build(self):
 		conn = PostgresConnector().get_connection() 
 		cursor = conn.cursor() 
-		query = 'select id,hashtags from "organizedTweets" limit 10'
+		query = 'select id,hashtags from "organizedTweets" '
 		cursor.execute(query)
 		id_column = 0
 		entities_column = 1
@@ -23,8 +24,43 @@ class EntityIdIndexer:
 					id_list = []
 					id_list.append(tweet_id)
 					entity_id_dict[entity] = id_list
+		return entity_id_dict			
 
-		print entity_id_dict
+	def build_tf(self):
+		# using group by first get tf score for each entity
+		conn = PostgresConnector().get_connection() 
+		cursor = conn.cursor() 
+		query = 'select count(id),entity from "IdEntity" group by entity'
+		cursor.execute(query)
+		count_of_id_column = 0
+		entities_column = 1
+		entity_id_dict = {}
+		for row in cursor:
+			count_of_id = row[count_of_id_column]
+			entity = row[entities_column]
+			entity_id_dict[entity] = count_of_id
 
-EntityIdIndexer().build()
+		return entity_id_dict			
 
+	def get_total_documents(self):
+		conn = PostgresConnector().get_connection() 
+		cursor = conn.cursor() 
+		query = 'select count(distinct(id)) from "IdEntity" '
+		cursor.execute(query)
+		count_of_distinct_id_column = 0
+		total_documents_count = 0
+		for row in cursor:
+			total_documents_count = row[count_of_distinct_id_column]
+
+		return total_documents_count
+
+
+
+
+
+###############
+EntityIdIndexObj = EntityIdIndexer()
+entity_count_id_dict = EntityIdIndexObj.build_tf()
+total_documents_count = EntityIdIndexObj.get_total_documents() 
+TfIdfObj = TfIdf()
+TfIdfObj.computeTfIdf(entity_count_id_dict,total_documents_count)

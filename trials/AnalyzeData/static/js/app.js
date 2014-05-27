0b7f1f2054d1993fa20a80e4ee0cdce9e51f03df
rd <nischal.hp@gmail.com>
2014-05-27 23:32:37 +0530

    Adding locations by reading it from DB
    * Fill the left nav nav list with locations
    * app.js is a customer js file that has methods to populate the same
    * index.html has the necessarym html elements added to facilitate this

---
+++ trials/AnalyzeData/static/js/app.js
@@ -0,0 +1,39 @@
+$( function(){
+	app = {
+			//init is the property of the object app which is
+			// represented as a function	
+			init : function(){
+				$.ajax({
+					type: "GET",
+					url: "/locations"
+				}).success(function(response){
+					app.onLocationSuccess(response);
+				});
+			},
+
+			onLocationSuccess : function(response){
+				var lineItems = '';
+				$.each(response.data, function(index,location){
+					var lineItem = '<li> <a href="#" class="location-items" data-geoid="'+location.geoid+'">'+location.city+'</a></li>';
+					lineItems = lineItems+lineItem;
+				});
+					$("#location-list").html(lineItems);
+			},
+
+			getTrends : function(geoid){
+
+			}
+
+
+
+		}}());
+
+app.init();
+
+$("#location-list").on('click',".location-items" , function(event){
+	$(".location-items").parent().removeClass("active");
+	$(this).parent().addClass("active");	
+	app.getTrends($(this).attr("data-geoid"));
+
+});
+

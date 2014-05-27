7886935f6fbbc1b2c9699fed21a233727efca532
rd <nischal.hp@gmail.com>
2014-05-27 23:34:33 +0530

    Added app route to handle locations

--- trials/AnalyzeData/app.py
+++ trials/AnalyzeData/app.py
@@ -1,4 +1,5 @@
-from flask import Flask,send_file
+from flask import Flask,send_file,jsonify,make_response
+from Pipeline import Pipeline
 app = Flask(__name__,instance_relative_config=True)
 app.config.from_pyfile('config.py')
 
@@ -7,6 +8,14 @@ app.config.from_pyfile('config.py')
 def hello_world():
 	return send_file('static/index.html')
 
+@app.route('/locations',methods=['GET'])
+def get_locations():
+	pipeline_obj = Pipeline();
+	locations_list = pipeline_obj.get_locations()
+	json_dict = {}
+	json_dict['data'] = locations_list	
+	print jsonify(json_dict)
+	return jsonify(json_dict)
 
 if __name__ == '__main__':
 	app.run(debug=app.config["DEBUG"])

a2ba098c4140ecfe7c00cb33f95ccc6084fd4531
rd <nischal.hp@gmail.com>
2014-05-31 23:41:19 +0530

    disintegrate_Tweets version 1 working

--- trials/AnalyzeData/Pipeline.py
+++ trials/AnalyzeData/Pipeline.py
@@ -4,8 +4,6 @@
 # then to extract the required columns
 # then create the id,entity table
 from PostgresConnector import PostgresConnector
-import pp
-
 
 class Pipeline:
 
@@ -49,19 +47,62 @@ class Pipeline:
 		return trends_list
 
 	def update_organized_tweets(self):
-		trends_list = []	
+		tweet_id_dict = {} 
+
+
 		try:
 			conn = PostgresConnector().get_connection()
 			cursor = conn.cursor()
-			query = 'select count(id) as "count",trend from "trends" group by trend'
+			query = 'select id from "trends"'
 			cursor.execute(query)
-			trend_column = 1
-			count_column = 0
+			trend_id_column = 0
 			for row in cursor:
-				trend_count = {}
-				trend_count["trend"] = row[trend_column]
-				trend_count["count"] = row[count_column]
-				trends_list.append(trend_count)
+				trend_id = row[trend_id_column]
+				query_tweets = 'select tweets from tweets where trendId = \''+trend_id+'\''
+				cursor_tweets = conn.cursor()
+				cursor_tweets.execute(query_tweets)
+				tweets_column = 0
+				for tweets_row in cursor_tweets:
+					tweets_json_array = tweets_row[tweets_column]
+					for json in tweets_json_array:
+						id = json['id']
+						tweet_id_exits = tweet_id_dict[id]
+						if tweet_id_exits is None:
+							tweet_id_dict[id] = 1
+							geo = json['geo']
+							retweeted = json['retweeted']
+							in_reply_to_screen_name = json['in_reply_to_screen_name']
+							truncated = json['truncated']
+							source = json['source']
+							created_at = json['created_at']
+							place = json['place']
+							user_id = json['user'][0]['id']
+							text = json['text']
+							entities = json['entities'][0]['hashtags']
+							user_mentions = json['entities'][0]['user_mentions']
+							retweet_count = json['retweet_count']
+							favorite_count = json['favorite_count']
+
+							print id,geo,retweeted,in_reply_to_screen_name,truncated,source,created_at,place,user_id,text,entities,user_mentions,retweet_count,favorite_count,trend_id
+
+						    # rest of the process to get all the fields and write to a file
+
+						else:
+							continue
+						break
+						# array of tweets json ends here
+
+					# total number of tweets rows for a given trend ends here
+					break
+
+				# all trends finish here
+				break
+				
+
+
+
+
+
 
 		except Exception as e:
 			print e 
@@ -70,7 +111,10 @@ class Pipeline:
 
 
 
-#pipeLine = Pipeline()
+pipeLine = Pipeline()
+pipeLine.update_organized_tweets()
 #list = pipeLine.get_locations()
+
+
 #print list
 

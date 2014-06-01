de92b21c990ca8b3a9739516d2710b437af1afb9
rd <nischal.hp@gmail.com>
2014-06-01 22:11:56 +0530

    Disintegration of tweets worksi :beers:
    * Used split to remove unwanted spaces between the text of the tweet
    * Added conn.commit() once import is done
    * Using with open
    * removal of the file once the whole process is done
    * using copy_From command

--- trials/AnalyzeData/Pipeline.py
+++ trials/AnalyzeData/Pipeline.py
@@ -4,6 +4,8 @@
 # then to extract the required columns
 # then create the id,entity table
 from PostgresConnector import PostgresConnector
+import traceback
+import os
 
 class Pipeline:
 
@@ -31,7 +33,7 @@ class Pipeline:
 		try:
 			conn = PostgresConnector().get_connection()
 			cursor = conn.cursor()
-			query = 'select trend,count from (select count(id) as "count",trend from "trends" where locationid = \''+location_id+'\' group by trend) as t1 order by count desc'
+			query = 'select trend,count from (select count(id) as "count",trend from "trends" where locationid = \''+str(location_id)+'\' group by trend) as t1 order by count desc'
 			cursor.execute(query)
 			trend_column = 0
 			count_column = 1
@@ -48,64 +50,67 @@ class Pipeline:
 
 	def update_organized_tweets(self):
 		tweet_id_dict = {} 
-
-
 		try:
 			conn = PostgresConnector().get_connection()
 			cursor = conn.cursor()
 			query = 'select id from "trends"'
 			cursor.execute(query)
 			trend_id_column = 0
-			for row in cursor:
-				trend_id = row[trend_id_column]
-				query_tweets = 'select tweets from tweets where trendId = \''+trend_id+'\''
-				cursor_tweets = conn.cursor()
-				cursor_tweets.execute(query_tweets)
-				tweets_column = 0
-				for tweets_row in cursor_tweets:
-					tweets_json_array = tweets_row[tweets_column]
-					for json in tweets_json_array:
-						id = json['id']
-						tweet_id_exits = tweet_id_dict[id]
-						if tweet_id_exits is None:
-							tweet_id_dict[id] = 1
-							geo = json['geo']
-							retweeted = json['retweeted']
-							in_reply_to_screen_name = json['in_reply_to_screen_name']
-							truncated = json['truncated']
-							source = json['source']
-							created_at = json['created_at']
-							place = json['place']
-							user_id = json['user'][0]['id']
-							text = json['text']
-							entities = json['entities'][0]['hashtags']
-							user_mentions = json['entities'][0]['user_mentions']
-							retweet_count = json['retweet_count']
-							favorite_count = json['favorite_count']
 
-							print id,geo,retweeted,in_reply_to_screen_name,truncated,source,created_at,place,user_id,text,entities,user_mentions,retweet_count,favorite_count,trend_id
+			with open('organized_data.txt','w') as f:
 
-						    # rest of the process to get all the fields and write to a file
+				for row in cursor:
+					trend_id = row[trend_id_column]
+					query_tweets = 'select tweets from tweets where trendId = \''+str(trend_id)+'\''
+					cursor_tweets = conn.cursor()
+					cursor_tweets.execute(query_tweets)
+					tweets_column = 0
 
-						else:
-							continue
+					for tweets_row in cursor_tweets:
+						tweets_json_array = tweets_row[tweets_column]
+
+						for json in tweets_json_array:
+							id = json['id']
+							tweet_id_exists = tweet_id_dict.get(id)
+							if tweet_id_exists is None:
+								#print jsonIn
+								tweet_id_dict[id] = 1
+								geo =  'none' if json['geo'] is None else json['geo']
+								retweeted = json['retweeted']
+								in_reply_to_screen_name = 'none' if json['in_reply_to_screen_name'] is None else json['in_reply_to_screen_name']
+								truncated = 'none' if json['truncated'] is None else json['truncated']
+								source = json['source']
+								created_at = json['created_at']
+								place = 'none' if json['place'] is None else json['place']
+								user_id = json['user']['id']
+								text = json['text'].encode('utf-8').strip()
+								text = " ".join(str(text).split())
+								entities = json['entities']['hashtags']
+								user_mentions = json['entities']['user_mentions']
+								retweet_count = json['retweet_count']
+								favorite_count = json['favorite_count']
+
+								#print geo,retweeted ,in_reply_to_screen_name ,truncated ,source ,created_at ,place ,user_id ,text ,entities ,user_mentions,retweet_count,favorite_count
+								f.write(str(id)+'\t'+str(geo.encode('utf-8'))+'\t'+str(retweeted)+'\t'+str(in_reply_to_screen_name.encode('utf-8'))+'\t'+str(truncated)+'\t'+str(source.encode('utf-8'))+'\t'+str(created_at.encode('utf-8'))+'\t'+str(place.encode('utf-8'))+'\t'+str(user_id)+'\t'+text+'\t'+str(entities)+'\t'+str(user_mentions)+'\t'+str(retweet_count)+'\t'+str(favorite_count)+'\t'+trend_id+'\n')
+
+							else:
+								continue
+							# array of tweets json ends here
+
+						# total number of tweets rows for a given trend ends here
 						break
-						# array of tweets json ends here
-
-					# total number of tweets rows for a given trend ends here
+					# all trends finish here
 					break
 
-				# all trends finish here
-				break
-				
+			print 'Writing to table'
+			with open('organized_data.txt') as f:
+				cursor.copy_from(f,'organized_tweets',columns=('id','geo','retweeted','in_reply_to_screen_name','truncated','source','created_at','place','user_id','text','entities','user_mentions','retweet_count','favorite_count','trend_id'))
+				conn.commit()
 
+			os.remove('organized_data.txt')
 
-
-
-
-
-		except Exception as e:
-			print e 
+		except Exception , err :
+			print traceback.format_exc()
 
 	    		
 

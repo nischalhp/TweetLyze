99298f5fe18bc23f02653c66b2fae11f2a3b9567
rd <nischal.hp@gmail.com>
2014-06-03 01:37:36 +0530

    Get trends call on selection of location handled
    * Return of trends for a particular location is working

--- trials/AnalyzeData/static/js/app.js
+++ trials/AnalyzeData/static/js/app.js
@@ -20,22 +20,27 @@ $( function(){
 					$("#location-list").html(lineItems);
 			},
 
+
 			getTrends : function(geoid){
+				alert("mofo");
 				$.ajax({
 					type: "GET",
-					
-				})
-
-			}
-
-
+					url : "/trends/"+geoid,
+					}).success(function(response){
+						console.log(response);
+					}).error(function(response){
+						console.log(response);
+					});
+				}
 
 		}}());
 
 app.init();
 
+/* Attaching click handler to location list */ 
 $("#location-list").on('click',".location-items" , function(event){
 	$(".location-items").parent().removeClass("active");
+	/* The click is on the anchor tag but we need make the whole line as active hence we uses this.parent*/
 	$(this).parent().addClass("active");	
 	app.getTrends($(this).attr("data-geoid"));
 

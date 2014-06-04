68d5382900228f39b579e7060f2fcb24a0c72706
rd <nischal.hp@gmail.com>
2014-06-04 17:49:11 +0530

    Changed the table name and used json loads
    * Retrieves a json array string from the table for entities
    * Using json.loads(str) gives a json array
    * changed IdEntity to id_entity

--- trials/AnalyzeData/IdEntityIndexer.py
+++ trials/AnalyzeData/IdEntityIndexer.py
@@ -3,7 +3,7 @@ import StringIO
 import os
 import sys
 from PostgresConnector import PostgresConnector
-
+import json
 class IdEntityIndexer:
 
     def build(self):
@@ -18,16 +18,17 @@ class IdEntityIndexer:
             for row in cursor:
                 tweet_id = row[id_column]
                 hashtag_array = row[entities_column]
-                hashtag_list = [ hashtag['text'] for hashtag in hashtag_array ]
+                json_array = json.loads(hashtag_array)
+                hashtag_list = [hashtag["text"] for hashtag in json_array]
                 hashtag_list_unique = list(set(hashtag_list))
+                print 'Writing data to table for the tweet_id ' +tweet_id 
                 for hashtag in hashtag_list_unique:
                     f.write(tweet_id + '\t' + hashtag.encode('utf-8') + '\n')
 
         with open('copy_from.txt') as f:
-            cursor.copy_from(f, '"IdEntity"', columns=('id', 'entity'))
+            cursor.copy_from(f, 'id_entity', columns=('id', 'entity'))
             conn.commit()
 
         os.remove('copy_from.txt')
 
-IdEntityIndexer().build()
 

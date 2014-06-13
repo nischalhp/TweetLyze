da310aba91516ea59ef907252a0c8bee2ba8e7dc
rd <nischal.hp@gmail.com>
2014-06-13 19:03:34 +0530

    Getting trend and entity list for a location

---
+++ trials/sqlProcedures/entity_trend_cluster.sql
@@ -0,0 +1,10 @@
+﻿select t1.entity,t2.trend from
+	(select id,entity from id_entity ) as t1
+	inner join
+	(select id,trend from organized_tweets where trend in 
+	(select trend from 
+	(select count(*) as c,trend from 
+	trends where locationid = '2295420' group by trend)as t_in order 
+	by c desc limit 15))as t2
+on
+t1.id = t2.id

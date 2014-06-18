8683c9e02137a162cc0a410eef20921c443db6b8
rd <nischal.hp@gmail.com>
2014-06-18 23:19:13 +0530

    get max and min date for trends in a location

---
+++ trials/sqlProcedures/get_dates_trend.sql
@@ -0,0 +1,5 @@
+﻿select max(date),min(date) from trends where trend in 
+		(select t1.trend as trend from
+		(select count(*) as c,trend from trends where 
+			locationid = '2295420' group by trend)as t1 order by c desc limit 15)
+			and locationid = '2295420'

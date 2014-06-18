8bba08f1e79d19f8cd0f0c50f96e544554cd13bb
rd <nischal.hp@gmail.com>
2014-06-18 23:17:39 +0530

    Added trend column to the table

--- trials/AnalyzeData/IdEntityIndexer.py
+++ trials/AnalyzeData/IdEntityIndexer.py
@@ -9,24 +9,26 @@ class IdEntityIndexer:
     def build(self):
         conn = PostgresConnector().get_connection()
         cursor = conn.cursor()
-        query = 'select id,entities from organized_tweets '
+        query = 'select id,entities,trend from organized_tweets'
         cursor.execute(query)
         id_column = 0
         entities_column = 1
+        trend_column = 2
 
         with open('copy_from.txt','w') as f:
             for row in cursor:
                 tweet_id = row[id_column]
+                trend = row[trend_column]
                 hashtag_array = row[entities_column]
                 json_array = json.loads(hashtag_array)
                 hashtag_list = [hashtag["text"] for hashtag in json_array]
                 hashtag_list_unique = list(set(hashtag_list))
                 print 'Writing data to table for the tweet_id ' +tweet_id 
                 for hashtag in hashtag_list_unique:
-                    f.write(tweet_id + '\t' + hashtag.encode('utf-8') + '\n')
+                    f.write(tweet_id + '\t' + hashtag.encode('utf-8') + '\t' + trend + '\n')
 
         with open('copy_from.txt') as f:
-            cursor.copy_from(f, 'id_entity', columns=('id', 'entity'))
+            cursor.copy_from(f, 'id_entity', columns=('id', 'entity','trend'))
             conn.commit()
 
         os.remove('copy_from.txt')

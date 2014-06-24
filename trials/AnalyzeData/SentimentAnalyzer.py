a9dc7b25d49e43f9fa9cc80b7739e6d9a29e7bf7
rd <nischal.hp@gmail.com>
2014-06-25 02:12:43 +0530

    Writing sentiments to Database

--- trials/AnalyzeData/SentimentAnalyzer.py
+++ trials/AnalyzeData/SentimentAnalyzer.py
@@ -8,10 +8,24 @@ class SentimentAnalyzer:
 		conn = PostgresConnector().get_connection()
 		cursor = conn.cursor()
 		query = """
-		select text from organized_tweets limit 10
+		select id,text from organized_tweets 
 		"""
 		cursor.execute(query)
+		id_column = 0
+		text_column = 1
 		for row in cursor:
-			text = row[0]
+			cursor_insert = conn.cursor()
+			text = row[text_column]
 			blob = TextBlob(text,analyzer=NaiveBayesAnalyzer())
-			print blob.sentiment
+			print "inserting sentiments for the text %s" % text
+			insert_query = """
+				update organized_tweets
+				set sentiment = %s,
+				pos_sentiment = %s,
+				neg_sentiment = %s
+				where id = %s
+
+			"""
+			cursor_insert.execute(insert_query,(blob.sentiment.classification,
+				blob.sentiment.p_pos,blob.sentiment.p_neg,row[id_column]))
+			conn.commit()	

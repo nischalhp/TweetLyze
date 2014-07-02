829efc760e26088e17b973d52e2731c5a698b8ee
rd <nischal.hp@gmail.com>
2014-07-02 23:36:13 +0530

    Testing cluster

--- trials/AnalyzeData/test_pipeline.py
+++ trials/AnalyzeData/test_pipeline.py
@@ -1,6 +1,6 @@
 from Pipeline import Pipeline 
 
 pipeline_obj = Pipeline()
-#pipeline_obj.update_organized_tweets()
-nodes_array,links_array,cluster_id_key = pipeline_obj.get_cluster('2295420')
-print nodes_array,links_array,cluster_id_key
+pipeline_obj.update_organized_tweets()
+# nodes_array,links_array,cluster_id_key = pipeline_obj.get_cluster('2295420')
+# print nodes_array,links_array,cluster_id_key
